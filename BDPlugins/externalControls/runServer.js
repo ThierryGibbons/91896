b8c71d0e64e2d92295afbec21756f6ae32e48f8a
Thierry <thierry.gibbons@gmail.com>
2020-08-31 17:47:44 +1200

    fixed async issue still not posting stuff yet

--- BDPlugins/externalControls/runServer.js
+++ BDPlugins/externalControls/runServer.js
@@ -1,4 +1,29 @@
+/* jshint esversion: 9 */
+
 var server = require('./comsWorldServer.js');
+
+var http = require("http");
 
 var httpServer = new server.Server();
 httpServer.listen();
+
+httpServer.post({body: "text here"});
+
+
+
+async function _post() {
+  const res = await httpServer.post({
+    hostname: 'sentry.io',
+    path: `/temp`,
+    headers: {
+      'Authorisation': `Bearer ${process.env.SENTRY_AUTH_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'production' : 'demo',
+    })
+  })
+  .catch(console.log("i caught it!!"));
+}
+
+_post();
