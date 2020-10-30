5f6cc3d5ed51e192391fee8982e53dfaa50fb7c1
Thierry <thierry.gibbons@gmail.com>
2020-10-31 00:34:49 +1300

    server now counts to 199 to ensure all devices can see console logs

--- BDPlugins/externalControls/runServer.js
+++ BDPlugins/externalControls/runServer.js
@@ -5,3 +5,9 @@ var server = require('./comsWorldServer.js');
 var httpServer = new server.Server();
 httpServer.listen();
 httpServer.discordRP("loading..");
+
+var i = 0;
+while (i < 200) {
+    console.log(i);
+    var i = i += 1;
+}
